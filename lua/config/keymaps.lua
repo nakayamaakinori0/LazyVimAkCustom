be76b8cb9d6e60f5b61213bbc158b199e65eecc0
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2025-05-10 15:04:51 +0800

    個人的なvimのキーバインドを追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- vim.keymap.set("n", "<leader>r", ":source $MYVIMRC")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>o", "o<Esc>")
+vim.keymap.set("n", "<leader>O", "O<Esc>")
+-- vim.keymap.set("n", "<leader>p>", ":echo expand("%:p")
