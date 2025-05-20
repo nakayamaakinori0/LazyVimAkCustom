ce166ac0a9b00043436bec1cafab92470f9da584
nakayama_a <nakayama_a@kokopelli-inc.com>
2025-05-20 18:52:32 +0900

    telekasten追加

---
+++ lua/plugins/telekasten.lua
@@ -0,0 +1,64 @@
+-- telekasten settings
+local private_home = vim.fn.expand("~/dev/telekasten/private")
+local work_home = vim.fn.expand("~/dev/telekasten/work")
+
+local daily_private = vim.fn.expand("~/dev/telekasten/private/daily")
+local weekly_private = vim.fn.expand("~/dev/telekasten/private/weekly")
+
+local daily_work = vim.fn.expand("~/dev/telekasten/work/daily")
+local weekly_work = vim.fn.expand("~/dev/telekasten/work/weekly")
+
+local templates_private = vim.fn.expand("~/dev/telekasten/private/templates")
+local template_new_note_private = vim.fn.expand("~/dev/telekasten/private/templates/default.md")
+local template_new_daily_private = vim.fn.expand("~/dev/telekasten/private/templates/daily.md")
+local template_new_weekly_private = vim.fn.expand("~/dev/telekasten/private/templates/weekly.md")
+
+local templates_work = vim.fn.expand("~/dev/telekasten/work/templates")
+local template_new_note_work = vim.fn.expand("~/dev/telekasten/work/templates/default.md")
+local template_new_daily_work = vim.fn.expand("~/dev/telekasten/work/templates/daily.md")
+local template_new_weekly_work = vim.fn.expand("~/dev/telekasten/work/templates/weekly.md")
+
+local private = {
+  home = private_home,
+  dailies = daily_private,
+  weeklies = weekly_private,
+  templates = templates_private,
+  template_new_note = template_new_note_private,
+  template_new_daily = template_new_daily_private,
+  template_new_weekly = template_new_weekly_private,
+}
+
+local work = {
+  home = work_home,
+  dailies = daily_work,
+  weeklies = weekly_work,
+  templates = templates_work,
+  template_new_note = template_new_note_work,
+  template_new_daily = template_new_daily_work,
+  template_new_weekly = template_new_weekly_work,
+}
+
+local vaults = {
+  work = work,
+  private = private,
+}
+
+return {
+  {
+    "renerocksai/telekasten.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {
+      home = private_home,
+      dailies = daily_private,
+      weeklies = weekly_private,
+      templates = templates_private,
+      template_new_note = template_new_note_private,
+      template_new_daily = template_new_daily_private,
+      template_new_weekly = template_new_weekly_private,
+      vaults = vaults,
+
+      auto_set_filetype = false,
+      filetype = "markdown",
+    },
+  },
+}
