7ebceeab651f54a08f336a7f5c6f6b3ec9decd92
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2025-05-24 23:32:06 +0900

    bookmark追加

--- lua/plugins/vim-bookmarks.lua
+++ lua/plugins/vim-bookmarks.lua
@@ -5,6 +5,9 @@ return {
     config = function()
       vim.g.bookmark_save_per_working_dir = 1
       vim.g.bookmark_auto_save = 1
+      vim.keymap.set("n", "<leader>mm", "<cmd>BookmarkToggle<cr>", { desc = "BookmarkToggle" })
+      vim.keymap.set("n", "<leader>mi", "<cmd>BookmarkAnnotate<cr>", { desc = "BookmarkAnnotate" })
+      vim.keymap.set("n", "<leader>ml", "<cmd>BookmarkShowAll<cr>", { desc = "BookmarkShowAll" })
     end,
   },
 }
