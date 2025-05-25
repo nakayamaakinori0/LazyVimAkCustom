40b67138ed8a43292bf79aac057123eb57c0d390
nakayama_a <nakayama_a@kokopelli-inc.com>
2025-05-25 16:49:56 +0900

    update diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,22 @@
 return {
-  { "sindrets/diffview.nvim" },
+  {
+    "sindrets/diffview.nvim",
+    -- tab, s-tabで次のファイルにいくdiffviewのdefault keymapを無効にしようとしたがうまくいかなかった（tab, s-tab自体が無効化されてしまう)
+    --[[
+    config = function()
+      require("diffview").setup({
+        keymaps = {
+          file_panel = {
+            ["<tab>"] = "<nop>",
+            ["<s-tab>"] = "<nop>",
+          },
+          view = {
+            ["<tab>"] = "<nop>",
+            ["<s-tab>"] = "<nop>",
+          },
+        },
+      })
+    end,
+    --]]
+  },
 }
