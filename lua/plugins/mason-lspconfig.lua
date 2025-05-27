a7f0cb67ec4ba652c22f0e16f3b375a378a8ea01
nakayama_a <nakayama_a@kokopelli-inc.com>
2025-05-27 14:40:21 +0900

    mason修正、phpの拡張やっぱやめる

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,3 +1,10 @@
 return {
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" }, -- 2.0.0で破壊的変更が入ったため,v1.x に固定
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0",               -- 2.0.0で破壊的変更が入ったため,v1.x に固定
+    opts = {
+      automatic_installation = false, -- 自動インストールを無効化
+      ensure_installed = {},          -- 空にするか必要なもののみ指定
+    },
+  },
 }
