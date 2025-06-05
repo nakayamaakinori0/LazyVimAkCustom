ab3730a275978721a3a702b0807fd12a6dbf8c85
nakayama_a <nakayama_a@kokopelli-inc.com>
2025-06-05 23:09:59 +0900

    intelephenseのフォーマットを無効化

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
   {
     "mason-org/mason-lspconfig.nvim",
-    version = "^1.0.0", -- 2.0.0で破壊的変更が入ったため,v1.x に固定
+    version = "^1.0.0",               -- 2.0.0で破壊的変更が入ったため,v1.x に固定
     opts = {
       automatic_installation = false, -- 自動インストールを無効化
       ensure_installed = {
@@ -13,4 +13,20 @@ return {
       "neovim/nvim-lspconfig",
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          settings = {
+            intelephense = {
+              format = {
+                enable = false, -- フォーマット機能を無効化
+              },
+            },
+          },
+        },
+      },
+    },
+  }
 }
