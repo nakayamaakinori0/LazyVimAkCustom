a2cc60f75a5c598c84966baa568788c8cdfbdf72
nakayama_a <nakayama_a@kokopelli-inc.com>
2025-06-07 13:00:51 +0900

    winresizerのデフォルトキーマップ無効化

--- lua/plugins/winresizer.lua
+++ lua/plugins/winresizer.lua
@@ -1,13 +1,17 @@
 return {
   "simeji/winresizer",
+
   keys = {
     { "<C-w>", "<cmd>WinResizerStartResize<cr>", desc = "Start window resize mode" },
   },
+
   config = function()
-    -- winresizerの設定
-    vim.g.winresizer_start_key = "<C-w>"
-    vim.g.winresizer_vert_resize = 5
-    vim.g.winresizer_horiz_resize = 3
+    -- ウィンドウサイズ変更の設定
+    vim.g.winresizer_vert_resize = 5  -- 垂直方向
+    vim.g.winresizer_horiz_resize = 3 -- 水平方向
     vim.g.winresizer_finish_with_escape = 1
+
+    -- デフォルトキーマップがinitで作成されてしまうので、configで削除する
+    vim.api.nvim_del_keymap('n', '<C-e>')
   end,
 }
