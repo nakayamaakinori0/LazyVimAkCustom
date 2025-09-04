9438b6e6bdb012acfed5735324e067596628083a
nakayama_a <nakayama_a@kokopelli-inc.com>
2025-09-04 19:32:57 +0900

    keymap整理、テストコード実行スクリプト削除jj

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,59 +25,30 @@ map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Buffers
-map("n", "<Tab>", ":BufferLineCycleNext<CR>")
-map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
-map("n", "<leader>h", ":BufferLineMovePrev<CR>")
-map("n", "<leader>l", ":BufferLineMoveNext<CR>")
-map("n", "<leader>bd", ":bdelete<CR>")
-map("n", "<leader>bD", ":BufferLinePickClose<CR>")
+map("n", "<leader>bl", ":Neotree buffers<CR>", { desc = "List Buffers" })
+map("n", "<leader>bD", ":% bd<CR>")
+
+-- file explorer
+map("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Explorer Toggle" })
+map("n", "<leader>E", ":Neotree reveal<CR>", { desc = "Explorer Toggle current file" })
 
 -- Range indent
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- Lazy
-map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
-
--- New file
-map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
-
 -- Location list
 map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
 map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
 
--- Quit
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
-
 -- Diagnostic keymaps
-map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 -- map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 -- map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-
--- Tabs
-map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
-map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
-
--- Highlights under cursor
-map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 
 -- Windows
 map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
 map("n", "<leader>ws", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>wv", "<C-W>v", { desc = "Split window right", remap = true })
--- map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
--- map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
-
--- Terminal mappings (window navigation defined in options.lua)
-map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
 -- Custom keymaps
 vim.keymap.set("i", "jj", "<Esc>")
@@ -89,9 +60,11 @@ vim.keymap.set("v", "H", "0")
 vim.keymap.set("v", "L", "$")
 
 vim.keymap.set("n", "<leader>tk", ":Telekasten panel<CR>")
+
 vim.keymap.set("n", "<leader>td", function()
   vim.cmd("normal! i" .. os.date("%Y-%m-%d"))
 end, { desc = "Insert current date (YYYY-MM-DD)" })
+
 vim.keymap.set("n", "<leader>gd", function()
   local diffview = require("diffview.lib")
   if diffview.get_current_view() then
