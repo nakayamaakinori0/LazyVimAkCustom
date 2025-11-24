182b1bbdca1f6080a7e5f9977ebc145c4e5ff7c0
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2025-11-24 22:39:35 +0900

    ステータスラインに相対パスを表示

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -9,5 +9,13 @@ return {
       globalstatus = true,
       disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
     },
+    sections = {
+      lualine_c = {
+        {
+          "filename",
+          path = 1, -- 0: ファイル名のみ, 1: 相対パス, 2: 絶対パス, 3: 絶対パス（~省略）, 4: 親ディレクトリ+ファイル名
+        },
+      },
+    },
   },
 }
