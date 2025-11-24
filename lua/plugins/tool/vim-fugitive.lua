c7694cde8e29affa8344451c2a03d22060d19338
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2025-11-24 22:39:53 +0900

    vim-fugitive keybind custom

--- lua/plugins/tool/vim-fugitive.lua
+++ lua/plugins/tool/vim-fugitive.lua
@@ -5,6 +5,8 @@ return {
     -- Optional: Set up any custom keybindings or configurations here
     vim.api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push<CR>", { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap("n", "<leader>gd", ":Gvdiffsplit<CR>", { noremap = true, silent = true })
   end,
 }
