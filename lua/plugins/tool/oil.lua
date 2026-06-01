ae9767f1fa2e7fc36635b84486e3c983dbee60f4
nakayama-akinori <nakayama_a@kokopelli-inc.com>
2026-06-01 09:14:26 +0900

    oilのソートを名前降順、タイプ昇順に変更

--- lua/plugins/tool/oil.lua
+++ lua/plugins/tool/oil.lua
@@ -3,7 +3,14 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      sort = {
+        { "type", "asc" },
+        { "name", "desc" },
+      },
+    },
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
