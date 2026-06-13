9f887fcfef7987797337906a541166c502d4c5c9
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2026-06-14 00:36:06 +0900

    nvim-tree sortを逆順に、vi .で開いた時ファイルツリーが開くようにする

--- lua/plugins/tool/nvim-tree.lua
+++ lua/plugins/tool/nvim-tree.lua
@@ -7,9 +7,21 @@ return {
     { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle file explorer" },
     { "<localleader>ef", ":NvimTreeFindFile<CR>", desc = "Find current file in file explorer" },
   },
+  lazy = false,
   opts = {
+    hijack_directories = {
+      enable = true,
+      auto_open = true,
+    },
     view = {
       width = 30,
+    },
+    sort = {
+      sorter = function(nodes)
+        table.sort(nodes, function(a, b)
+          return a.name > b.name
+        end)
+      end,
     },
   },
 }
